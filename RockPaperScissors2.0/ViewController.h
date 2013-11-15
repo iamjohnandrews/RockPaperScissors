0e66a9d731686d193298df376339f34a4a23b637
John Andrews <johnnydrews@gmail.com>
2013-11-15 13:11:58 -0600

    add u elements to storyboard

---
+++ RockPaperScissors2.0/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RockPaperScissors2.0
+//
+//  Created by John Andrews on 11/12/13.
+//  Copyright (c) 2013 John Andrews. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ RockPaperScissors2.0Tests/RockPaperScissors2_0Tests.m
@@ -0,0 +1,34 @@
+//
+//  RockPaperScissors2_0Tests.m
+//  RockPaperScissors2.0Tests
+//
+//  Created by John Andrews on 11/12/13.
+//  Copyright (c) 2013 John Andrews. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RockPaperScissors2_0Tests : XCTestCase
+
+@end
+
+@implementation RockPaperScissors2_0Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
