6a933e238f63518e975b0b0fcbae2b95aff1ada9
John Andrews <johnnydrews@gmail.com>
2013-11-17 16:38:28 -0600

    clear all effects when new game starts

--- RockPaperScissors2.0/ViewController.m
+++ RockPaperScissors2.0/ViewController.m
@@ -64,7 +64,12 @@
 - (IBAction)startButtonPressed:(id)sender {
     //1. clear all labels
     playerHandLabel.text = @"Player's results";
+    playerHandLabel.layer.backgroundColor = [[UIColor blackColor]CGColor];
+    playerHandLabel.layer.borderColor = [[UIColor blackColor]CGColor];
+    
     computerHandLabel.text = @"CPU's results";
+    computerHandLabel.layer.backgroundColor = [[UIColor blackColor]CGColor];
+    computerHandLabel.layer.borderColor = [[UIColor blackColor]CGColor];
     
     //2. countDownLabel displays countdown clock
     timer = [NSTimer scheduledTimerWithTimeInterval:1.0f target:self selector:@selector(countdownClock) userInfo:nil repeats:YES];
@@ -83,7 +88,8 @@
         }
         count = 4;
         computerHandLabel.text = [choices objectAtIndex:arc4random() % 3];
-        NSLog(@"playerHandLabel value = %@", playerHandLabel.text);
+        [self winner];
+        //NSLog(@"playerHandLabel value = %@", playerHandLabel.text);
     }
 }
 
@@ -95,7 +101,31 @@
 - (void)winner
 {
     //5. Have the background and border of winner change
-    
+    if ([computerHandLabel.text isEqualToString:@"Rock"] && [playerHandLabel.text isEqualToString:@"Scissors"]) {
+        computerHandLabel.layer.borderColor = [[UIColor cyanColor]CGColor];
+        computerHandLabel.layer.borderWidth = 2.0f;
+        computerHandLabel.layer.backgroundColor = [[UIColor blueColor]CGColor];
+    } else if ([computerHandLabel.text isEqualToString:@"Paper"] && [playerHandLabel.text isEqualToString:@"Rock"]){
+        computerHandLabel.layer.borderColor = [[UIColor cyanColor]CGColor];
+        computerHandLabel.layer.borderWidth = 2.0f;
+        computerHandLabel.layer.backgroundColor = [[UIColor blueColor]CGColor];
+    } else if ([computerHandLabel.text isEqualToString:@"Scissors"] && [playerHandLabel.text isEqualToString:@"Paper"]){
+        computerHandLabel.layer.borderColor = [[UIColor cyanColor]CGColor];
+        computerHandLabel.layer.borderWidth = 2.0f;
+        computerHandLabel.layer.backgroundColor = [[UIColor blueColor]CGColor];
+    } else if ([playerHandLabel.text isEqualToString:@"Rock"] && [computerHandLabel.text isEqualToString:@"Scissors"]) {
+        playerHandLabel.layer.borderColor = [[UIColor cyanColor]CGColor];
+        playerHandLabel.layer.borderWidth = 2.0f;
+        playerHandLabel.layer.backgroundColor = [[UIColor blueColor]CGColor];
+    } else if ([playerHandLabel.text isEqualToString:@"Paper"] && [computerHandLabel.text isEqualToString:@"Rock"]){
+        playerHandLabel.layer.borderColor = [[UIColor cyanColor]CGColor];
+        playerHandLabel.layer.borderWidth = 2.0f;
+        playerHandLabel.layer.backgroundColor = [[UIColor blueColor]CGColor];
+    } else if ([playerHandLabel.text isEqualToString:@"Scissors"] && [computerHandLabel.text isEqualToString:@"Paper"]){
+        playerHandLabel.layer.borderColor = [[UIColor cyanColor]CGColor];
+        playerHandLabel.layer.borderWidth = 2.0f;
+        playerHandLabel.layer.backgroundColor = [[UIColor blueColor]CGColor];
+    }
 
 }
 
